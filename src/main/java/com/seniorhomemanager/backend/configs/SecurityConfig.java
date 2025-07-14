6764343228eb69dec6e84d596326f7fe1d220c5b
Calin Ciocanea <c.ciocanea@student.tudelft.nl>
2025-07-14 17:29:09 +0200

    Add CORS config file

---
+++ src/main/java/com/seniorhomemanager/backend/configs/SecurityConfig.java
@@ -0,0 +1,2 @@
+package com.seniorhomemanager.backend.configs;public class SecurityConfig {
+}
---
+++ src/main/java/com/seniorhomemanager/backend/controller/HelloWorldController.java
@@ -0,0 +1,2 @@
+package com.seniorhomemanager.backend.controller;public class HelloWorldController {
+}
