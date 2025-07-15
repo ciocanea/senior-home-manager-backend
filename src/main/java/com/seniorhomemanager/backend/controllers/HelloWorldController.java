536287429241257cd629264961c38540d951f957
Calin Ciocanea <c.ciocanea@student.tudelft.nl>
2025-07-15 15:33:48 +0200

    Changed controller to communicate with service

--- src/main/java/com/seniorhomemanager/backend/controllers/HelloWorldController.java
+++ src/main/java/com/seniorhomemanager/backend/controllers/HelloWorldController.java
@@ -1,15 +1,33 @@
-package com.seniorhomemanager.backend.controller;
+package com.seniorhomemanager.backend.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.seniorhomemanager.backend.models.HelloWorldModel;
+import com.seniorhomemanager.backend.services.HelloWorldService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello-world")
 public class HelloWorldController {
 
+    private final HelloWorldService helloWorldService;
+
+    @Autowired
+    public HelloWorldController(HelloWorldService helloWorldService) {
+        this.helloWorldService = helloWorldService;
+    }
+
     @GetMapping("/hello-world")
     public String helloWorld() {
-        return "Hello, World!";
+        return helloWorldService.helloWorld();
+    }
+
+    @GetMapping("/hello-world-repository")
+    public String helloWorldRepository() {
+        return helloWorldService.helloWorldRepository();
+    }
+
+    @PostMapping("/create-hello-world")
+    public String createHelloWorld(@RequestBody HelloWorldModel helloWorldModel) {
+        return helloWorldService.createHelloWorld(helloWorldModel);
     }
 }
