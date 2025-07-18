c08bf19aa418830901ee8bea5ced6ac2fd8a8009
Calin Ciocanea <c.ciocanea@student.tudelft.nl>
2025-07-18 14:43:13 +0200

    Implemented beneficiary entity and repository

--- src/main/java/com/seniorhomemanager/backend/models/Beneficiary.java
+++ src/main/java/com/seniorhomemanager/backend/models/Beneficiary.java
@@ -1,2 +1,36 @@
-package com.seniorhomemanager.backend.models;public class Beneficiary {
+package com.seniorhomemanager.backend.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Beneficiary {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String nume;
+    private String prenume;
+
+    private String cnp;
+    private String serieCi;
+    private String numarCi;
+
+//    private String locNastere;
+//
+//    private String domiciliu;
+//
+//    private String sex;
+
+
 }
--- src/main/java/com/seniorhomemanager/backend/repositories/BeneficiaryRepository.java
+++ src/main/java/com/seniorhomemanager/backend/repositories/BeneficiaryRepository.java
@@ -1,2 +1,11 @@
-package com.seniorhomemanager.backend.repositories;public interface BeneficiaryRepository {
+package com.seniorhomemanager.backend.repositories;
+
+import com.seniorhomemanager.backend.models.Beneficiary;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface BeneficiaryRepository extends JpaRepository<Beneficiary, UUID> {
 }
