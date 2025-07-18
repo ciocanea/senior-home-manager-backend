e5079c7b7974193330bff4a2a2a838d6c6429891
Calin Ciocanea <c.ciocanea@student.tudelft.nl>
2025-07-18 14:41:48 +0200

    Implemented beneficiary post and get endpoints

--- src/main/java/com/seniorhomemanager/backend/services/BeneficiaryService.java
+++ src/main/java/com/seniorhomemanager/backend/services/BeneficiaryService.java
@@ -1,2 +1,25 @@
-package com.seniorhomemanager.backend.services;public class BeneficiaryService {
+package com.seniorhomemanager.backend.services;
+
+import com.seniorhomemanager.backend.models.Beneficiary;
+import com.seniorhomemanager.backend.repositories.BeneficiaryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BeneficiaryService {
+
+    private final BeneficiaryRepository beneficiaryRepository;
+
+    public BeneficiaryService (BeneficiaryRepository beneficiaryRepository) {
+        this.beneficiaryRepository = beneficiaryRepository;
+    }
+
+    public List<Beneficiary> getAll () {
+        return beneficiaryRepository.findAll();
+    }
+
+    public Beneficiary add (Beneficiary beneficiary) {
+        return beneficiaryRepository.save(beneficiary);
+    }
 }
