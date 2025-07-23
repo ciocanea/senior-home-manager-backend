52ea15b2e7e6d360eaa52f57ceaf9e355d003b09
Calin Ciocanea <c.ciocanea@student.tudelft.nl>
2025-07-23 15:12:36 +0200

    Implement document controller, service and request DTO to replace placeholders and get documents

--- src/main/java/com/seniorhomemanager/backend/DTOs/DocumentRequestDTO.java
+++ src/main/java/com/seniorhomemanager/backend/DTOs/DocumentRequestDTO.java
@@ -1,2 +1,10 @@
-package com.seniorhomemanager.backend.DTOs;public class DocumentRequestDTO {
+package com.seniorhomemanager.backend.DTOs;
+
+import com.seniorhomemanager.backend.models.Beneficiary;
+import lombok.Data;
+
+@Data
+public class DocumentRequestDTO {
+    private String templateName;
+    private Beneficiary beneficiary;
 }
--- src/main/java/com/seniorhomemanager/backend/services/DocumentService.java
+++ src/main/java/com/seniorhomemanager/backend/services/DocumentService.java
@@ -1,2 +1,39 @@
-package com.seniorhomemanager.backend.services;public class DocumentService {
+package com.seniorhomemanager.backend.services;
+
+import com.seniorhomemanager.backend.models.Beneficiary;
+import com.seniorhomemanager.backend.utils.TemplateFiller;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Map;
+
+@Service
+public class DocumentService {
+
+    private final TemplateFiller templateFiller;
+
+    @Value("${template.folder.path:data/templates}")
+    private String templateFolderPath;
+
+
+    public DocumentService(TemplateFiller templateFiller) {
+        this.templateFiller = templateFiller;
+    }
+
+    public byte[] generate (String templateName, Map<String, String> placeholderValues) throws IOException {
+        File templateFile = new File(templateFolderPath, templateName);
+
+        if (!templateFile.exists() || !templateFile.isFile()) {
+            throw new IllegalArgumentException("Template not found: " + templateName);
+        }
+
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+            templateFiller.fillTemplate(templateFile, outputStream, placeholderValues);
+            return outputStream.toByteArray();
+        }
+    }
 }
