6921ba747ba61f65eae5ac70647305af5953fe3f
Calin Ciocanea <c.ciocanea@student.tudelft.nl>
2025-07-28 16:20:32 +0200

    Implement document retreival and placeholder replacement.

--- src/main/java/com/seniorhomemanager/backend/controllers/DocumentController.java
+++ src/main/java/com/seniorhomemanager/backend/controllers/DocumentController.java
@@ -2,7 +2,9 @@ package com.seniorhomemanager.backend.controllers;
 
 import com.seniorhomemanager.backend.DTOs.DocumentRequestDTO;
 import com.seniorhomemanager.backend.models.Beneficiary;
+import com.seniorhomemanager.backend.services.BeneficiaryService;
 import com.seniorhomemanager.backend.services.DocumentService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -13,40 +15,35 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/documents")
 public class DocumentController {
 
     private final DocumentService documentService;
+    private final BeneficiaryService beneficiaryService;
 
     @Autowired
-    public DocumentController (DocumentService documentService) {
+    public DocumentController (DocumentService documentService, BeneficiaryService beneficiaryService) {
         this.documentService = documentService;
+        this.beneficiaryService = beneficiaryService;
     }
 
     @PostMapping("/generate")
     public ResponseEntity<byte[]> generate (@RequestBody DocumentRequestDTO documentRequestDTO) {
         try {
             String templateName = documentRequestDTO.getTemplateName();
-            Beneficiary beneficiary = documentRequestDTO.getBeneficiary();
+            Beneficiary beneficiary = beneficiaryService.get(documentRequestDTO.getBeneficiaryId());
 
-            String currentDate = LocalDate.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-
-            Map<String, String> placeholderValues = Map.of(
-                    "${data}", currentDate,
-                    "${nume}", beneficiary.getNume(),
-                    "${prenume}", beneficiary.getPrenume()
-            );
-
-            byte[] document = documentService.generate(templateName, placeholderValues);
+            byte[] document = documentService.generate(templateName, beneficiary);
 
             return ResponseEntity.ok()
                     .contentType(MediaType.APPLICATION_OCTET_STREAM)
                     .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + templateName + "\"")
                     .body(document);
 
-        } catch (IllegalArgumentException e) {
+        } catch (IllegalArgumentException | EntityNotFoundException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
